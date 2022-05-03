83f0ec5d7105c32bbd856550be1eabe06cfdf65d
Dany Knight <gnuwaves@gmail.com>
2022-05-03 13:32:39 -0400

    Add click events that change color

--- script.js
+++ script.js
@@ -1,18 +1,37 @@
 
 
-//Grid Container Set Up
 
+//Color Change function
+
+
+//Grid Container Set Up
 
 for (var i = 0; i < 16; i++) {
         var row = document.createElement('div');
         row.className = "row";
-        for (var j = 0; j < 16; j++) {
-            var box = document.createElement('div');
-            box.className = "box" + i + j;
+        for (let j = 0; j < 16; j++) {
+            let box = document.createElement('div');
+            box.className = "box";
             row.appendChild(box);
+            let boxNumber = [i]+[j];
+            box.setAttribute('id',boxNumber)
         }                
         document.getElementById('boxParent').appendChild(row);
-    }
+}
+
+
+//Add click event listener to change box color
+
+const boxes = document.getElementsByClassName("box"); //returns an array-like object
+
+const changeColor = function() {
+    this.style.backgroundColor = "blue"; //turns 'this' element blue
+};
+
+for (let i = 0; i < boxes.length; i++) {
+    boxes[i].addEventListener('click', changeColor); //iterates through box 'array' adding event to each
+}
+
 
 
     //Grid Creator
